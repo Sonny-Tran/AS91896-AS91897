f24fcbdaa31db9c1dbe6d9ff105aff034f50585d
Sonny-Tran <112600183+Sonny-Tran@users.noreply.github.com>
2023-05-06 19:54:31 +1200

    Add files via upload

--- Julie_birthday_hire.py
+++ Julie_birthday_hire.py
@@ -1,12 +1,65 @@
 from tkinter import *
-from Julie_birthday_hire_modules import modules
-
+import tkinter.font as tkFont
 
 root = Tk()
+root.configure(bg="grey")
+
+
+### Font Configuration Start
+
+title_font = tkFont.Font(family="Helvetica",size=24,weight="bold")
+
+### Font Configuration End
 
 
 
-win_x, win_y, margin_x, margin_y = modules.window_geometery()
+### Frame Creation Start
+
+
+title_frame = Frame(root, padx=15, pady=15)
+entries_frame = Frame(root, padx=15, pady=15)
+buttons_frame = Frame(root, padx=15, pady=15)
+
+title_frame.grid(column=0, row=0, sticky="ew")
+entries_frame.grid(column=0, row=1, sticky="ew")
+buttons_frame.grid(column=0, row=2, sticky="ew")
+
+
+### Frame Creation End
+
+
+### Title Start
+
+Label(title_frame, text="Julie_Birthday_Hire", font=title_font)
+
+### Title Start
+
+
+### Entries Boxes Start
+
+
+
+
+
+
+
+### Entries Boxes End
+
+
+
+### Tkinter Window Configuration Start
+screen_x, screen_y = root.winfo_screenwidth(), root.winfo_screenheight() #Grabs device resolution
+
+
+# Window Geometry Calculations
+win_x = int(screen_x / 3) # Deciding window width based on screen size, so no matter what display, window will always take up 33.333%
+win_y = int(screen_y / 3) # Deciding window height based on screen size, so no matter what display, window will always take up 33.333%
+
+
+margin_x = int(screen_x / 2 - win_x / 2) # Adding padding so window will always appear in the middle
+margin_y = int(screen_y / 2 - win_y / 2) # Screen_height - Window_Height = Empty space on the side / 2 since we want the window to be in the middle
+
 root.geometry(f"{win_x}x{win_y}+{margin_x}+{margin_y}")
+### Tkinter Window Configuration End
 
 mainloop()

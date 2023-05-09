a571c4230f68ad209f9ea6ff9e58994bf09334aa
Sonny-Tran <112600183+Sonny-Tran@users.noreply.github.com>
2023-05-09 15:14:05 +1200

    Add files via upload

--- Julie_birthday_hire.py
+++ Julie_birthday_hire.py
@@ -1,8 +1,10 @@
 from tkinter import *
 import tkinter.font as tkFont
+from tkinter import messagebox
 
 win = Tk()
 win.configure(bg="grey")
+
 
 # Functions Start
 
@@ -16,8 +18,51 @@ def unfocused(event, msg):
         event.widget.configure(fg="grey")
         event.widget.insert(0, msg)
 
+def submit():
+    customer_name = customer_name_entry.get()
+    recipt_number = recipt_number_entry.get()
+    item_hired = item_hired_entry.get()
+    number_of_items = num_of_item_hired_entry.get()
+
+    submittion_list = []
+    submittion_list.append(customer_name)
+    submittion_list.append(recipt_number)
+    submittion_list.append(item_hired)
+    submittion_list.append(number_of_items)
+
+    if customer_name == "Enter Your Name":
+        messagebox.showerror(title="Name Error", message="Please enter your full name")
+
+    elif recipt_number == "Enter Your Receipt Number":
+        messagebox.showerror(title="Receipt Number Error", message="Please Enter a receipt number")
+
+    elif item_hired == "Enter The Item Hired":
+        messagebox.showerror(title="Item Error", message="Please Enter the item hired")
+
+    elif number_of_items == "Enter The Amount Of Items Hired":
+        messagebox.showerror(title="No. of Item Error", message="Please Enter the amount of items hired")
+
+    
+
+    print(customer_name, recipt_number, item_hired, number_of_items)
+
+def new_window():
+    global win2
+    try:
+
+        if win2.winfo_exists():
+            win2.deiconify()
+        else:
+            win2 = Toplevel()
+            set_up(win2)
+
+    except:
+        win2 = Toplevel()
+        set_up(win2)
 
 
+def set_up(window):
+    pass
 
 
 # Functions End
@@ -96,12 +141,19 @@ num_of_item_hired_entry.grid(row=1, column=3, pady=(15, 0))
 
 
 ### Buttons Start
+
 buttons_frame.columnconfigure((0,2,4,6), weight=1)
 buttons_frame.rowconfigure((0,2), weight=1)
 
 
 quit_button = Button(buttons_frame, text="Quit", font=button_font, command=win.quit)
 quit_button.grid(row=1, column=1)
+
+submit_entries = Button(buttons_frame, text="Submit", font=button_font, command=submit)
+submit_entries.grid(row=1, column=3)
+
+view_entries = Button(buttons_frame, text="View Entries", font=button_font, command=new_window)
+view_entries.grid(row=1, column=5)
 
 ### Buttons End
 
@@ -120,5 +172,6 @@ margin_y = int(screen_y / 2 - win_y / 2) # Screen_height - Window_Height = Empty
 
 win.geometry(f"{win_x}x{win_y}+{margin_x}+{margin_y}")
 ### Tkinter Window Configuration End
+
 
 mainloop()
